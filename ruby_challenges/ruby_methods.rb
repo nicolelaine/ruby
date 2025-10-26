a15f78086330974459a03283e9b60a916bddfaf5
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2025-10-26 16:43:52 +0100

    Refactored with agruments

--- ruby_challenges/ruby_methods.rb
+++ ruby_challenges/ruby_methods.rb
@@ -1,3 +1,4 @@
+=begin
 def method 
     puts "This is a Ruby method"
 end 
@@ -9,3 +10,13 @@ def addition
 end 
 
 addition
+=end
+
+def addition_method(num1, num2)
+    number = num1 + num2
+    puts number
+end 
+
+addition_method(10, 10)
+addition_method(100, 2)
+addition_method(30, 5)
