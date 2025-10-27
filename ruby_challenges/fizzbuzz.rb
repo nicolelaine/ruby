a49952bb6952b18904dd80cd44caa8829476513f
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2025-10-27 21:07:45 +0100

    Added fizzbuzz challenge

---
+++ ruby_challenges/fizzbuzz.rb
@@ -0,0 +1,14 @@
+number = 1
+
+while number <=100
+    if number % 5 == 0 && number % 3 == 0
+        puts "fizzbuzz"
+    elsif number % 3 == 0
+         puts "fizz"
+    elsif number % 5 == 0
+         puts "buzz"
+    else
+         puts number 
+    end 
+    number +=1
+end 
